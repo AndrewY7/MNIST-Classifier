36379b675bd12ba170ea0f053608fcc358471470
Andyrewy7 <89996690+Andyrewy7@users.noreply.github.com>
2024-01-12 17:56:40 -0500

    First Commit: Importing Frameworks

---
+++ classifier.py
@@ -0,0 +1,11 @@
+import numpy as np         
+import pandas as pd
+import matplotlib.pyplot as plt      
+import random                        
+
+from keras.datasets import mnist     
+from keras.models import Sequential  
+
+from keras.layers.core import Dense, Dropout, Activation 
+from keras.utils import np_utils 
+
