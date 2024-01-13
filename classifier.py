03a5d1e6a0371959e3531ed3c13a97719c8abbb9
Andyrewy7 <89996690+Andyrewy7@users.noreply.github.com>
2024-01-12 23:57:07 -0500

    first commit

--- classifier.py
+++ classifier.py
@@ -1,5 +1,5 @@
 import numpy as np         
-import pandas as pd
+#import pandas as pd
 import matplotlib.pyplot as plt      
 import random                        
 
@@ -9,3 +9,8 @@ from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation 
 from keras.utils import np_utils 
 
+(X_train, Y_train), (X_test, Y_test) = mnist.load_data()
+print("X_train shape", X_train.shape)
+print("y_train shape", y_train.shape)
+print("X_test shape", X_test.shape)
+print("y_test shape", y_test.shape)
